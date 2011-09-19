2e2856aeb848cefbb2953f4900e02a8e9101082c
Nils Maier <MaierMan@web.de>
2011-09-20 01:09:40 +0200

    Cache twitter and stub caching of news
    The latter is done to get a more realistic view of the page performance
    in development.
    Closes #1

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,14 @@
 module HomeHelper
   def favorite_tweets
-    Twitter::Client.new.favorites("steffoz")[0..3]
+    Rails.cache.fetch("home/twitter", :expires_in => 2.hours) do 
+      Twitter::Client.new.favorites("steffoz")[0..3]
+    end
   end
 
   def latest_news
-    feed = FeedNormalizer::FeedNormalizer.parse open("http://www.downthemall.net/feed/rss/")
-    feed.entries[0..2]
+    Rails.cache.fetch("home/latestnews", :expires_in => 2.hours) do
+      feed = FeedNormalizer::FeedNormalizer.parse open("http://www.downthemall.net/feed/rss/")
+      feed.entries[0..2]
+    end
   end
 end
