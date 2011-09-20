9b79c0ac572a8ef6680d3bdc03b549e153f195b3
Stefano Verna <stefano.verna@welaika.com>
2011-09-21 00:18:36 +0200

    Created devise User model

---
+++ config/initializers/rspec.rb
@@ -0,0 +1,3 @@
+Rails.application.config.generators do |g|
+  g.test_framework :rspec, fixture_replacement: :factory_girl
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Downthemall::Application.routes.draw do
+  devise_for :users
+
   root to: "home#index"
 end
