cffeb224187e23452e7657b2d09744d225c439c4
Nils Maier <MaierMan@web.de>
2011-09-20 05:50:09 +0200

    Add workaround for slammed twitter caches

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,18 +1,27 @@
 module HomeHelper
   def favorite_tweets
     rv = Rails.cache.fetch("home/twitter", :expires_in => 2.hours) do 
-      Twitter::Client.new.favorites("steffoz")[0..3]
+      fetch_tweets
     end
-    logger.info rv
+    # FIXME: Find out WTF? is going on with <Hashie.Mash> serialization
+    # when the cache gets slammed
+    if not rv.respond_to?('each')
+      logger.warn "twitter cache did not deserialize! was: #{rv}"
+      rv = fetch_tweets
+      Rails.cache.write "home/twitter", rv, :expires_in => 2.hours
+    end
     rv
   end
 
   def latest_news
-    rv = Rails.cache.fetch("home/latestnews", :expires_in => 2.hours) do
+    Rails.cache.fetch("home/latestnews", :expires_in => 2.hours) do
       feed = FeedNormalizer::FeedNormalizer.parse open("http://www.downthemall.net/feed/rss/")
       feed.entries[0..2]
     end
-    logger.info rv
-    rv
+  end
+
+  private
+  def fetch_tweets
+    Twitter::Client.new.favorites("steffoz")[0..3]
   end
 end
