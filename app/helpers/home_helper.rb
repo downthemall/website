1246d26fb484cf5e4335af6ba613e783e01f0b70
Nils Maier <MaierMan@web.de>
2011-09-20 05:17:26 +0200

    Log the cache stuff (debug)

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,14 +1,18 @@
 module HomeHelper
   def favorite_tweets
-    Rails.cache.fetch("home/twitter", :expires_in => 2.hours) do 
+    rv = Rails.cache.fetch("home/twitter", :expires_in => 2.hours) do 
       Twitter::Client.new.favorites("steffoz")[0..3]
     end
+    logger.info rv
+    rv
   end
 
   def latest_news
-    Rails.cache.fetch("home/latestnews", :expires_in => 2.hours) do
+    rv = Rails.cache.fetch("home/latestnews", :expires_in => 2.hours) do
       feed = FeedNormalizer::FeedNormalizer.parse open("http://www.downthemall.net/feed/rss/")
       feed.entries[0..2]
     end
+    logger.info rv
+    rv
   end
 end
