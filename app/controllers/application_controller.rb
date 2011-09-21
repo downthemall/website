d87c3ed5539ca15a5f9d66004c4e70b71887ebdb
Stefano Verna <stefano.verna@welaika.com>
2011-09-21 23:20:40 +0200

    Better looking auth pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
 module ApplicationHelper
 
-  def page_id(*args)
-    (@_page_id ||= []).concat(args).compact.join(" ")
+  def form_error_messages(resource)
+    if resource.errors.present?
+      content_tag :ul, {:class => "form_errors"} do
+        resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join.html_safe
+      end
+    else
+      ""
+    end
   end
 
 end
