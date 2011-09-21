34ff7cd6d247f88898b189a4f3506b2c4088f2e6
Stefano Verna <stefano.verna@welaika.com>
2011-09-22 00:03:42 +0200

    Adding knowledge base Article

---
+++ spec/acceptance/knowledge_base/home_spec.rb
@@ -0,0 +1,9 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Knowledge Base Home', %q{
+  In order to know better DownThemAll!
+  As a website visitor
+  I want to be able to navigate throught the KB documentation
+} do
+
+end
