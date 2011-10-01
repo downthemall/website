c1e9c41efa859edab19c6e345b327d76658ad0bf
Stefano Verna <stefano.verna@welaika.com>
2011-10-01 09:41:38 +0200

    Closes #8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Downthemall::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup' }
   resources :articles do
     resources :translations, :controller => :article_translations, :only => :show
   end
