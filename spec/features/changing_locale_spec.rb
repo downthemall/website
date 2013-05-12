d39552cc7ab679b16ca1e9cad9549e85d3049e69
Stefano Verna <stefano.verna@welaika.com>
2013-05-12 12:33:05 +0200

    Update gems and fix specs

--- spec/features/changing_locale_spec.rb
+++ spec/features/changing_locale_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 feature 'Changing locale' do
   scenario 'User changes locale' do
+    AmoStats.stub(:new).and_return(stub.as_null_object)
+
     visit '/en'
     I18n.with_locale(:en) do
       expect(page).to have_content I18n.t('homepage.title')
