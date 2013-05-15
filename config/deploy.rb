a43bf584eb58f608dc84361fdcddd1832d1e18b1
Stefano Verna <stefano.verna@welaika.com>
2013-05-15 12:14:10 +0200

    Add tmp folder

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ task :deploy => :environment do
     invoke :'rails:assets_precompile'
 
     to :launch do
-      queue 'touch tmp/restart.txt'
+      queue "touch #{deploy_to}/current/tmp/restart.txt"
     end
   end
 end
