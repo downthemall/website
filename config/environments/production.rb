25c4e20b735354f0fa832f7124b5518330b869af
Stefano Verna <stefano.verna@welaika.com>
2013-05-15 12:16:41 +0200

    Add assets to precompilation phase

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Downthemall::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( markdown_base.css markdown_editor.css markdown_preview.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false  config.action_mailer.delivery_method = :smtp
